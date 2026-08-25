76d101f7f8f4ae65df573a0d81590422982b8d56
Hinali0802 <henitandel08@gmail.com>
2026-08-25 17:16:18 +0530

    Add files via upload

---
+++ 26BCP309_1#.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    while(1)
+    {
+        printf("enter a no:");
+        scanf("%d",&a);
+        if(a==10)
+        {
+            break;
+        }
+    }
+    return 0;
+}
